f78b213ea14e848bff9794d2c250887497c439cf
tiankuo-android <1226147264@qq.com>
2017-05-31 20:22:48 +0800

    第五次上传--使用butterknife实例化

--- app/src/main/java/com/atguigu/tiankuo/recyclerviewdemo/adapter/NetRecyclerAdapter.java
+++ app/src/main/java/com/atguigu/tiankuo/recyclerviewdemo/adapter/NetRecyclerAdapter.java
@@ -5,12 +5,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.atguigu.tiankuo.recyclerviewdemo.R;
 import com.atguigu.tiankuo.recyclerviewdemo.domain.NetAudioBean;
 import com.atguigu.tiankuo.recyclerviewdemo.util.Utils;
 import com.squareup.picasso.Picasso;
+
+import org.xutils.x;
 
 import java.util.List;
 
@@ -26,21 +29,20 @@ public class NetRecyclerAdapter extends RecyclerView.Adapter{
     private final Context mContext;
     private final List<NetAudioBean.ListBean> datas;
 
+    public NetRecyclerAdapter(Context mContext, List<NetAudioBean.ListBean> datas) {
+        this.mContext = mContext;
+        this.datas = datas;
+    }
+
     private static final int TYPE_VIDEO = 0;
     private static final int TYPE_IMAGE = 1;
     private static final int TYPE_TEXT = 2;
     private static final int TYPE_GIF = 3;
     private static final int TYPE_AD = 4;
 
-    public NetRecyclerAdapter(Context mContext, List<NetAudioBean.ListBean> datas) {
-        this.mContext = mContext;
-        this.datas = datas;
-    }
-
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return initViewHolder(viewType);
-
     }
 
     private RecyclerView.ViewHolder initViewHolder(int viewType) {
@@ -52,23 +54,23 @@ public class NetRecyclerAdapter extends RecyclerView.Adapter{
                 viewHolder = new VideoHoder(convertView);
 
                 break;
-//            case TYPE_IMAGE://图片
-//                convertView = View.inflate(mContext, R.layout.all_image_item, null);
-//                viewHolder = new ImageHolder(convertView);
-//                break;
-//            case TYPE_TEXT://文字
-//                convertView = View.inflate(mContext, R.layout.all_text_item, null);
-//                viewHolder = new TextHolder(convertView);
-//                break;
-//            case TYPE_GIF://gif
-//                convertView = View.inflate(mContext, R.layout.all_gif_item, null);
-//                viewHolder = new GifHolder(convertView);
-//
-//                break;
-//            case TYPE_AD://软件广告
-//                convertView = View.inflate(mContext, R.layout.all_ad_item, null);
-//                viewHolder = new ADHolder(convertView);
-//                break;
+            case TYPE_IMAGE://图片
+                convertView = View.inflate(mContext, R.layout.all_image_item, null);
+                viewHolder = new ImageHolder(convertView);
+                break;
+            case TYPE_TEXT://文字
+                convertView = View.inflate(mContext, R.layout.all_text_item, null);
+                viewHolder = new TextHolder(convertView);
+                break;
+            case TYPE_GIF://gif
+                convertView = View.inflate(mContext, R.layout.all_gif_item, null);
+                viewHolder = new GifHolder(convertView);
+
+                break;
+            case TYPE_AD://软件广告
+                convertView = View.inflate(mContext, R.layout.all_ad_item, null);
+                viewHolder = new ADHolder(convertView);
+                break;
         }
         return viewHolder;
 
@@ -80,19 +82,21 @@ public class NetRecyclerAdapter extends RecyclerView.Adapter{
         if (getItemViewType(position) == TYPE_VIDEO) {
             VideoHoder videoHoder = (VideoHoder) holder;
             videoHoder.setData(datas.get(position));
-//        } else if (getItemViewType(position) == TYPE_IMAGE) {
-//            ImageHolder imageHolder = (ImageHolder) holder;
-//            imageHolder.setData(datas.get(position));
-//        } else if (getItemViewType(position) == TYPE_TEXT) {
-//            TextHolder textHolder = (TextHolder) holder;
-//            textHolder.setData(datas.get(position));
-//        } else if (getItemViewType(position) == TYPE_GIF) {
-//            GifHolder gifHolder = (GifHolder) holder;
-//            gifHolder.setData(datas.get(position));
-//        } else {
-//            ADHolder adHolder = (ADHolder) holder;
-//            adHolder.setData(datas.get(position));
-       }
+        } else if (getItemViewType(position) == TYPE_IMAGE) {
+            ImageHolder imageHolder = (ImageHolder) holder;
+            imageHolder.setData(datas.get(position));
+        } else if (getItemViewType(position) == TYPE_TEXT) {
+            TextHolder textHolder = (TextHolder) holder;
+            textHolder.setData(datas.get(position));
+        } else if (getItemViewType(position) == TYPE_GIF) {
+            GifHolder gifHolder = (GifHolder) holder;
+            gifHolder.setData(datas.get(position));
+        } else {
+            ADHolder adHolder = (ADHolder) holder;
+            adHolder.setData(datas.get(position));
+        }
+
+
     }
 
     @Override
@@ -100,7 +104,7 @@ public class NetRecyclerAdapter extends RecyclerView.Adapter{
         return 0;
     }
 
-    class VideoHoder extends RecyclerView.ViewHolder {
+    private class VideoHoder extends BaseViewHolder {
         Utils utils;
         TextView tvContext;
         JCVideoPlayerStandard jcvVideoplayer;
@@ -109,10 +113,8 @@ public class NetRecyclerAdapter extends RecyclerView.Adapter{
         ImageView ivCommant;
         TextView tvCommantContext;
 
-        VideoHoder(View convertView) {
+        public VideoHoder(View convertView) {
             super(convertView);
-//            super(convertView);
-            //中间公共部分 -所有的都有
             tvContext = (TextView) convertView.findViewById(R.id.tv_context);
             utils = new Utils();
             tvPlayNums = (TextView) convertView.findViewById(R.id.tv_play_nums);
@@ -121,23 +123,121 @@ public class NetRecyclerAdapter extends RecyclerView.Adapter{
             tvCommantContext = (TextView) convertView.findViewById(R.id.tv_commant_context);
             jcvVideoplayer = (JCVideoPlayerStandard) convertView.findViewById(R.id.jcv_videoplayer);
         }
+        public void setData(NetAudioBean.ListBean mediaItem) {
+            super.setData(mediaItem);
 
-        public void setData(NetAudioBean.ListBean listBean) {
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
 
-            tvContext.setText(listBean.getText() + "_" + listBean.getType());
             boolean setUp = jcvVideoplayer.setUp(
-                    listBean.getVideo().getVideo().get(0), JCVideoPlayer.SCREEN_LAYOUT_LIST,
+                    mediaItem.getVideo().getVideo().get(0), JCVideoPlayer.SCREEN_LAYOUT_LIST,
                     "");
             if (setUp) {
                 Picasso.with(mContext)
-                        .load(listBean.getVideo().getThumbnail().get(0))
+                        .load(mediaItem.getVideo().getThumbnail().get(0))
                         .placeholder(R.drawable.video_default)
                         .error(R.drawable.video_default)
                         .into(jcvVideoplayer.thumbImageView);
             }
-            tvPlayNums.setText(listBean.getVideo().getPlaycount() + "次播放");
-            tvVideoDuration.setText(utils.stringForTime(listBean.getVideo().getDuration() * 1000) + "");
+            tvPlayNums.setText(mediaItem.getVideo().getPlaycount() + "次播放");
+            tvVideoDuration.setText(utils.stringForTime(mediaItem.getVideo().getDuration() * 1000) + "");
 
         }
     }
+
+    private class ImageHolder extends RecyclerView.ViewHolder {
+        public ImageHolder(View convertView) {
+            super(convertView);
+        }
+
+        public void setData(NetAudioBean.ListBean listBean) {
+
+        }
+    }
+
+    private class TextHolder extends RecyclerView.ViewHolder {
+        public TextHolder(View convertView) {
+            super(convertView);
+        }
+
+        public void setData(NetAudioBean.ListBean listBean) {
+
+        }
+    }
+
+    private class GifHolder extends RecyclerView.ViewHolder {
+        public GifHolder(View convertView) {
+            super(convertView);
+        }
+
+        public void setData(NetAudioBean.ListBean listBean) {
+
+        }
+    }
+
+    private class ADHolder extends RecyclerView.ViewHolder {
+        public ADHolder(View convertView) {
+            super(convertView);
+        }
+
+        public void setData(NetAudioBean.ListBean listBean) {
+
+        }
+    }
+
+    private class BaseViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivHeadpic;
+        TextView tvName;
+        TextView tvTimeRefresh;
+        ImageView ivRightMore;
+        ImageView ivVideoKind;
+        TextView tvVideoKindText;
+        TextView tvShenheDingNumber;
+        TextView tvShenheCaiNumber;
+        TextView tvPostsNumber;
+        LinearLayout llDownload;
+
+        public BaseViewHolder(View convertView) {
+            super(convertView);
+
+            ivHeadpic = (ImageView) convertView.findViewById(R.id.iv_headpic);
+            tvName = (TextView) convertView.findViewById(R.id.tv_name);
+            tvTimeRefresh = (TextView) convertView.findViewById(R.id.tv_time_refresh);
+            ivRightMore = (ImageView) convertView.findViewById(R.id.iv_right_more);
+
+            ivVideoKind = (ImageView) convertView.findViewById(R.id.iv_video_kind);
+            tvVideoKindText = (TextView) convertView.findViewById(R.id.tv_video_kind_text);
+            tvShenheDingNumber = (TextView) convertView.findViewById(R.id.tv_shenhe_ding_number);
+            tvShenheCaiNumber = (TextView) convertView.findViewById(R.id.tv_shenhe_cai_number);
+            tvPostsNumber = (TextView) convertView.findViewById(R.id.tv_posts_number);
+            llDownload = (LinearLayout) convertView.findViewById(R.id.ll_download);
+        }
+
+        public void setData(NetAudioBean.ListBean mediaItem) {
+            if (mediaItem.getU() != null && mediaItem.getU().getHeader() != null && mediaItem.getU().getHeader().get(0) != null) {
+                x.image().bind(ivHeadpic, mediaItem.getU().getHeader().get(0));
+            }
+            if (mediaItem.getU() != null && mediaItem.getU().getName() != null) {
+                tvName.setText(mediaItem.getU().getName() + "");
+            }
+
+            tvTimeRefresh.setText(mediaItem.getPasstime());
+
+            List<NetAudioBean.ListBean.TagsBean> tagsEntities = mediaItem.getTags();
+            if (tagsEntities != null && tagsEntities.size() > 0) {
+                StringBuffer buffer = new StringBuffer();
+                for (int i = 0; i < tagsEntities.size(); i++) {
+                    buffer.append(tagsEntities.get(i).getName() + " ");
+                }
+                tvVideoKindText.setText(buffer.toString());
+            }
+
+            tvShenheDingNumber.setText(mediaItem.getUp());
+            tvShenheCaiNumber.setText(mediaItem.getDown() + "");
+            tvPostsNumber.setText(mediaItem.getForward() + "");
+
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/atguigu/tiankuo/recyclerviewdemo/pager/NetRecyclerPager.java
+++ app/src/main/java/com/atguigu/tiankuo/recyclerviewdemo/pager/NetRecyclerPager.java
@@ -1,18 +1,31 @@
 package com.atguigu.tiankuo.recyclerviewdemo.pager;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.atguigu.tiankuo.recyclerviewdemo.R;
 import com.atguigu.tiankuo.recyclerviewdemo.fragment.BaseFragment;
 
 import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class NetRecyclerPager extends BaseFragment {
 
     private ArrayList<String> datas;
+    @Bind(R.id.recyclerview)
+    RecyclerView recyclerView;
+    @Bind(R.id.progressbar)
+    ProgressBar progressbar;
+    @Bind(R.id.tv_nomedia)
+    TextView tvNomedia;
     @Override
     public View initView() {
         View view = View.inflate(context, R.layout.activity_net_recycler_pager, null);
+        ButterKnife.bind(this, view);
         return null;
     }
 
@@ -24,5 +37,9 @@ public class NetRecyclerPager extends BaseFragment {
         }
     }
 
-
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
